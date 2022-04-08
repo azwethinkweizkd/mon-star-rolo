742f151b6ef4e47e77e9c9886eab98fc3371a554
Kevin Devlin <azwethinkweizkd@gmail.com>
2022-04-07 21:12:32 -0600

    Converted App class component to a funtional, changed props on other components

--- src/App.js
+++ src/App.js
@@ -1,43 +1,37 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { CardList } from "./components/card-list/card-list.component";
 import { SearchBox } from "./components/search-box/search-box.components";
 import "./App.css";
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      monsters: [],
-      searchField: "",
-    };
-  }
+const App = () => {
+  const [monsters, setMonsters] = useState([]);
+  const [searchField, setSearchField] = useState("");
+  const [filteredMonsters, setFilterMonsters] = useState(monsters);
 
-  componentDidMount() {
+  useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
-      .then((users) => this.setState({ monsters: users }));
-  }
+      .then((users) => setMonsters(users));
+  }, []);
 
-  handleChange = (e) => {
-    this.setState({ searchField: e.target.value });
-  };
-
-  render() {
-    const { monsters, searchField } = this.state;
-    const filteredMonsters = monsters.filter((monster) =>
+  useEffect(() => {
+    const newFilteredMonsters = monsters.filter((monster) =>
       monster.name.toLowerCase().includes(searchField.toLowerCase())
     );
-    return (
-      <div className="App">
-        <h1>Monster Rolodex</h1>
-        <SearchBox
-          placeholder="Search Monsters"
-          handleChange={this.handleChange}
-        />
-        <CardList monsters={filteredMonsters} />
-      </div>
-    );
-  }
-}
+    setFilterMonsters(newFilteredMonsters);
+  }, [monsters, searchField]);
+
+  const handleChange = (e) => {
+    setSearchField(e.target.value);
+  };
+
+  return (
+    <div className="App">
+      <h1>Monster Rolodex</h1>
+      <SearchBox placeholder="Search Monsters" handleChange={handleChange} />
+      <CardList monsters={filteredMonsters} />
+    </div>
+  );
+};
 
 export default App;
--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -3,10 +3,10 @@ import { Card } from "../card/card.component";
 
 import "./card-list.styles.css";
 
-export const CardList = (props) => {
+export const CardList = ({ monsters }) => {
   return (
     <div className="card-list">
-      {props.monsters.map((monster) => (
+      {monsters.map((monster) => (
         <Card key={monster.id} monster={monster} />
       ))}
     </div>
